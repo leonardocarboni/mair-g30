1bd1ec5b6627ea25bc3b77442dd12e624498ca29
Merel8 <65536982+Merel8@users.noreply.github.com>
2022-09-15 10:42:53 +0200

    baseline 2 ready to test

--- INFOMAIR Project 1a.py
+++ INFOMAIR Project 1a.py
@@ -29,13 +29,15 @@ Baseline 1:
 A baseline system that, regardless of the content of the utterance, 
 always assigns the majority class of in the data.
 """
-majority = Y_train.mode()[0]
-while True:
-    prompt = input()
-    if not prompt:
-        break
-    else:
-        print(majority)
+# =============================================================================
+# majority = Y_train.mode()[0]
+# while True:
+#     prompt = input()
+#     if not prompt:
+#         break
+#     else:
+#         print(majority)
+# =============================================================================
     
 """
 Baseline 2:
@@ -44,35 +46,53 @@ A baseline rule-based system based on keyword matching.
 
 classes = {
     'ack': ['ok', 'okay'],
-    'affirm': ['yes', 'yeah', 'yep', 'sure', 'ok', 'okay'],
+    'affirm': ['yes', 'yeah', 'yep', 'sure', 'ok', 'okay', 'right', 'indeed'],
     'bye': ['bye', 'goodbye', 'see you', 'see you later', 'talk to you later'],
-    'confirm': [],
+    'confirm': ['is it', 'is there', 'is that', 'does it', 'do they'],
     'deny': ['dont', 'don\'t', 'do not'],
     'hello': ['hello', 'hi', 'hey', 'good morning', 'good afternoon', 'good evening'],
-    'inform': ['look', 'looking for'],
+    'inform': ['look', 'looking for', 'search', 'find'],
     'negate': ['no'],
     'null': ['cough', 'clear throat', 'laugh', 'sigh', 'sniff'],
     'repeat': ['again', 'repeat', 'what was that', 'what did you say', 'what did you mean'],
-    'reqalts': ['alternatives', 'other options', 'other choices', 'other suggestions'],
-    'reqmore': ['more', 'another', 'different', 'else', 'other'],
-    'request': ["?"],
+    'reqalts': ['how about', 'alternatives', 'other options', 'other choices', 'other suggestions', \
+                'another', 'different', 'else', 'other', 'anything else'],
+    'reqmore': ['more'],
+    'request': ["?", "what"],
     'restart': ['start over', 'restart', 'start again', 'start from the beginning'],
     'thankyou': ['thank you', 'thanks', 'thank you very much', 'thanks very much'],
 }
-while True:
-    prompt = input()
-    if not prompt:
-        break
-    else:
-        for key, value in classes.items():
+majority = Y_train.mode()[0]
+# =============================================================================
+# while True:
+#     prompt = input()
+#     if not prompt:
+#         break
+#     else:
+#         for key, value in classes.items():
+#             for v in value:
+#                 if v in prompt:
+#                     print(key)
+#                     break
+#                 else:
+#                     print(majority)
+# =============================================================================
+        
+
+"""
+Testing baseline 2.
+"""
+count = 0
+for i, x in enumerate(X_test):
+    for key, value in classes.items():
             for v in value:
-                if v in prompt:
-                    print(key)
-                    break
-            else:
-                continue
-            break
-        else:
-            print(majority)
+                if v in x:
+                    if key == Y_test.iloc[i]:
+                        count += 1
+                        break
+                    else:
+                        if majority == Y_test.iloc[i]:
+                            count += 1
+print(count/len(X_test))
 
 
