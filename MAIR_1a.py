5cbe027dd7d1e47f05e65c1d353a0250c005af16
Leonardo Carboni <43882851+leonardocarboni@users.noreply.github.com>
2022-09-15 15:04:34 +0200

    fixed little bugs, comments and formatting

--- MAIR_1a.py
+++ MAIR_1a.py
@@ -4,31 +4,37 @@ Part 1a: text classification
 Group G30 (Giacomo Bais, Leonardo Carboni, Merel de Goede, Merel van den Bos)
 """
 
-from sklearn.model_selection import train_test_split
 import pandas as pd
-dialog_act = []
-utterance_content = []
+from sklearn.model_selection import train_test_split
 
+# Creating the dataframe
 d = pd.read_csv('dialog_acts.dat', header=None)
 df = pd.DataFrame(data=d)
 df.columns = ['dialog_act']
 
+# Splitting the dataframe columns
 df[['dialog_act', 'utterance_content']
    ] = df.dialog_act.str.split(' ', 1, expand=True)
 
+# Lowercasing the content
+df['dialog_act'] = df['dialog_act'].str.lower()
+df['utterance_content'] = df['utterance_content'].str.lower()
 
+# Splitting the dataframe into train and test
 X_train, X_test, Y_train, Y_test = train_test_split(df['utterance_content'],
                                                     df['dialog_act'],
                                                     test_size=0.15,
                                                     random_state=42)
 
+# Majority class
 majority = Y_train.mode()[0]
 
+# Classes Dictionary
 classes = {
-    'ack': ['ok', 'okay', "fine", "all right"],
-    'affirm': ['yes', 'yeah', 'yep', 'sure', 'ok', 'okay', 'right', 'indeed'],
+    'ack': ['ok', 'okay', "fine"],
+    'affirm': ['yes', 'yeah', 'yep', 'sure', 'right', 'indeed'],
     'bye': ['bye', 'goodbye', 'see you', 'see you later', 'talk to you later'],
-    'confirm': ['is it', 'is there', 'is that', 'does it', 'do they'],
+    'confirm': ['it is', 'there is', 'it does', 'they do', 'it is true',],
     'deny': ['dont', 'don\'t', 'do not', 'does not', 'is not', 'was not', 'were not', 'are not', 'cannot', 'can not', 'cant', 'can\'t', 'no', 'nope', 'not', 'never', 'none', 'nobody', 'nothing', 'nowhere', 'neither', 'nor', 'never', 'no one', 'none', 'not a single', 'not any', 'not any more', 'not at all', 'not either', 'not at all', 'not ever', 'not in the least', 'not less than', 'not more than', 'not at all', 'not quite', 'not so', 'not yet', 'hardly', 'scarcely', 'barely', 'rarely', 'seldom', 'little', 'less', 'least', 'few', 'fewer', 'fewest', 'short', 'shorter', 'shortest', 'low', 'lower', 'lowest', 'small', 'smaller', 'smallest', 'tiny', 'tiniest', 'young', 'younger', 'youngest', 'little', 'less', 'least', 'few', 'fewer', 'fewest', 'short', 'shorter', 'shortest', 'low', 'lower', 'lowest', 'small', 'smaller', 'smallest', 'tiny', 'tiniest', 'young', 'younger', 'youngest', 'little', 'less', 'least', 'few', 'fewer', 'fewest', 'short', 'shorter', 'shortest', 'low', 'lower', 'lowest', 'small', 'smaller', 'smallest', 'tiny', 'tiniest', 'young', 'younger', 'youngest', 'little', 'less', 'least', 'few', 'fewer', 'fewest', 'short', 'shorter', 'shortest', 'low', 'lower', 'lowest', 'small', 'smaller', 'smallest', 'tiny', 'tiniest', 'young', 'younger', 'youngest', 'little', 'less', 'least', 'few', 'fewer', 'fewest', 'short', 'shorter', 'shortest', 'low', 'lower', 'lowest', 'small', 'smaller', 'smallest', 'tiny', ],
     'hello': ['hello', 'hi', 'hey', 'good morning', 'good afternoon', 'good evening'],
     'inform': ['look', 'looking for', 'search', 'find', 'want', 'need', 'require', 'requirement', 'requirements', 'want to', 'need to', 'require to', 'requirement to', 'requirements to', 'want to find', 'need to find', 'require to find', 'requirement to find', 'requirements to find', 'want to look', 'need to look', 'require to look', 'requirement to look', 'requirements to look', 'want to search', 'need to search', 'require to search', 'requirement to search', 'requirements to search', 'want to find', 'need to find', 'require to find', 'requirement to find', 'requirements to find', 'want to look', 'need to look', 'require to look', 'requirement to look', 'requirements to look', 'want to search', 'need to search', 'require to search', 'requirement to search', 'requirements to search', 'want to find', 'need to find', 'require to find', 'requirement to find', 'requirements to find', 'want to look', 'need to look', 'require to look', 'requirement to look', 'requirements to look', 'want to search', 'need to search', 'require to search', 'requirement to search', 'requirements to search', 'want to find', 'need to find', 'require to find', 'requirement to find', 'requirements to find', 'want to look', 'need to look', 'require to look', 'requirement to look', 'requirements to look', 'want to search', 'need to search', 'require to search', 'requirement to search', 'requirements to search', 'want to find', 'need to find', 'require to find', 'requirement to find', 'requirements to find', 'want to look', 'need to look', 'require to look', 'requirement to look', 'requirements to look', 'want to search', 'need to search', 'require to search', 'requirement to search', 'requirements to search', 'want to find', 'need to find', 'require to find', 'requirement to find', 'requirements to find', 'want to look', 'need to look', 'require to look', 'requirement to look', 'requirements to look', 'want to search', 'need to search', 'require to search', 'requirement to search', ],
@@ -44,12 +50,16 @@ classes = {
 }
 
 
+# Main Loop
 while True:
+
+    # User Input
     print("\nChoose what you want to do:")
     print("1. Baseline 1")
     print("2. Baseline 2")
     print("3. Test")
     print("\n0. Exit")
+
     choice = input("Enter your choice: ")
 
     if choice == "1":
@@ -60,7 +70,8 @@ while True:
         """
         print("\nBaseline 1 - Write a sentence or a word")
 
-        prompt = input()
+        # Reading input and converting it in lower case
+        prompt = input().lower()
         print(majority)
 
     elif choice == "2":
@@ -70,11 +81,19 @@ while True:
         """
         print("\nBaseline 2 - Write a sentence or a word")
 
-        prompt = input()
+        # Reading input and converting it in lower case
+        prompt = input().lower()
+
+        nKeys = 0
         for key, value in classes.items():
             for v in value:
                 if v in prompt:
                     print(key)
+                    nKeys += 1
+                    break
+        if nKeys == 0:
+            print(majority)
+
     elif choice == "3":
         """
         Testing baseline 2.
